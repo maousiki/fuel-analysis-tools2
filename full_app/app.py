c53d50eb27bf153653b0c500b5cdfd0c87bf87cf
maousiki <maousiki@yahoo.co.jp>
2025-04-16 14:31:56 +0900

    app.py

--- full_app/app.py
+++ full_app/app.py
@@ -69,6 +69,8 @@ if uploaded_file is not None:
     xaxis_title=None
 ),
     xaxis_title=None
+),
+    xaxis_title=None
 ), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None)
         st.plotly_chart(fig1, use_container_width=True)
 
@@ -83,6 +85,8 @@ if uploaded_file is not None:
     xaxis_title=None
 ),
     xaxis_title=None
+),
+    xaxis_title=None
 ), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None), xaxis_title=None)
         st.plotly_chart(fig2, use_container_width=True)
 
@@ -92,6 +96,8 @@ if uploaded_file is not None:
     xaxis={'tickangle': -45},
     yaxis_range=[0, None],
     margin=dict(l=0, r=0, t=30, b=150),
+    xaxis_title=None
+),
     xaxis_title=None
 ),
     xaxis_title=None
